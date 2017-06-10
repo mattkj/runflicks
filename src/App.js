955e2c0a38d8956d6882f301eeb7401f5f11b0a8
Matt Johnson <computermonkey@gmail.com>
2017-06-10 16:00:17 -0700

    connect to YouTube api with google-client-api

--- src/App.js
+++ src/App.js
@@ -3,6 +3,32 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  componentDidMount(){
+    require( 'google-client-api' )().then((gapi) => {
+      gapi.client.init({
+        'apiKey': process.env.REACT_APP_API_KEY,
+        'discoveryDocs': ['https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest'] 
+      }).then(() => {
+        console.log('GAPI Initialized.');
+        return gapi.client.youtube.search.list({
+          maxResults: '3',
+          part: 'snippet',
+          q: 'running',
+          type: 'video'
+        })
+      }).then((response) => {
+        response.result.items.forEach((item) => {
+          console.group();
+          console.log(item.id.videoId);
+          console.log(item.snippet);
+          console.groupEnd();
+        });
+      }, (error) => {
+        console.warn('Error: ' + error);
+      });
+    });
+  }
+  
   render() {
     return (
       <div className="App">
