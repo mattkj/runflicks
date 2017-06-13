67c2f8bb8346a904c576b7b27084fd6f2f3a0dfe
Matt Johnson <computermonkey@gmail.com>
2017-06-12 23:18:43 -0700

    add React Router, About & Home components

--- src/components/App.js
+++ src/components/App.js
@@ -1,57 +1,21 @@
-import React, { Component } from 'react';
-import {youTube} from '../utils/gapi';
-import Loading from './Loading';
-import Thumbnails from './Thumbnails';
-// import EmbedVideo from './EmbedVideo';
+import React from 'react';
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom'
+import Home from './Home';
+import About from './About';
 
-const videoIds = ['Ks-_Mh1QhMc','c0KYU2j0TM4','eIho2S0ZahI'];
-
-class App extends Component {
-  constructor(){
-    super();
-    this.state = {
-      videos: '',
-      loading: true,
-    }
-  }
-
-  async componentDidMount(){
-    // const videos = await youTube.search('3', 'trail running', 'video');
-    const videos = await youTube.getVideos(videoIds.join());
-    this.setState({
-      videos: videos.result.items,
-      loading: false
-    });
-  }
-  
-  render() {
-    let content;
-    const videoResults = this.state.videos;
-    const loading = this.state.loading;
-
-    if (loading === true) {
-      return <Loading />
-    } else {
-      content = videoResults.map(item => {
-        return <Thumbnails 
-                  key={item.id}
-                  title={item.snippet.title} 
-                  src={item.snippet.thumbnails.maxres.url} 
-                />
-        // return  <EmbedVideo 
-        //           key={item.id}
-        //           id={item.id}
-        //           title={item.snippet.title} 
-        //         />
-      });
-    }
-
+const App = () => {
     return (
-      <div className="content">
-        {content}
-      </div>
+      <Router>
+        <div>
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/about">About</Link></li>
+          </ul>
+          <Route exact path='/' component={Home} />
+          <Route path='/about' component={About} />
+        </div>
+      </Router>
     );
-  }
 }
 
 export default App;
---
+++ src/components/Home.js
@@ -0,0 +1,59 @@
+import React, { Component } from 'react';
+import {youTube} from '../utils/gapi';
+import Loading from './Loading';
+import Thumbnails from './Thumbnails';
+// import EmbedVideo from './EmbedVideo';
+
+const videoIds = ['VyNWVdExM24','A66ierfTCUQ','8DoARSlv-HU'];
+
+class Home extends Component {
+  constructor(){
+    super();
+    this.state = {
+      videos: '',
+      loading: true,
+    }
+  }
+
+  async componentDidMount(){
+    // const videos = await youTube.search('3', 'trail running', 'video');
+    const videos = await youTube.getVideos(videoIds.join());
+    this.setState({
+      videos: videos.result.items,
+      loading: false
+    });
+  }
+  
+  render() {
+    let content;
+    const videoResults = this.state.videos;
+    const loading = this.state.loading;
+
+    if (loading === true) {
+      return <Loading />
+    } else {
+      content = videoResults.map(item => {
+        let src = (item.snippet.thumbnails.maxres ? item.snippet.thumbnails.maxres.url : item.snippet.thumbnails.medium.url);
+        return <Thumbnails 
+                  key={item.id}
+                  title={item.snippet.title} 
+                  src={src} 
+                />
+        // return  <EmbedVideo 
+        //           key={item.id}
+        //           id={item.id}
+        //           title={item.snippet.title} 
+        //         />
+      });
+    }
+
+    return (
+      <div className="content">
+        <h1>Home</h1>
+        {content}
+      </div>
+    );
+  }
+}
+
+export default Home;
