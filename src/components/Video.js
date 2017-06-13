e1015127f4093ac6b519e4666fe4cafb322f60d5
Matt Johnson <matt@bamsas.com>
2017-06-13 12:25:15 -0700

    adjust youtube embed url

--- src/components/Video.js
+++ src/components/Video.js
@@ -4,12 +4,12 @@ import queryString from 'query-string';
 
 function Video({match, location}) {
   const title = queryString.parse(location.search).title;
-  
+
   return(
     <div>
       <h3>{title}</h3>
       <div className='video-responsive'>
-        <iframe width="640" height="360" src={`https://www.youtube.com/embed/${match.params.id}`} title={title} frameBorder="0" allowFullScreen></iframe>
+        <iframe width="640" height="360" src={`https://www.youtube.com/embed/${match.params.id}?rel=0&amp;showinfo=0`} title={title} frameBorder="0" allowFullScreen></iframe>
       </div>
     </div>
   )
