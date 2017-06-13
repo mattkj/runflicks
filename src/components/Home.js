d3fed3c06313d10602aeee4041b2b2940ab8616b
Matt Johnson <matt@bamsas.com>
2017-06-13 16:33:13 -0700

    filterVideos function

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,9 @@ import {youTube} from '../utils/gapi';
 import Loading from './Loading';
 import Thumbnails from './Thumbnails';
 
-const videoIds = ['VyNWVdExM24','A66ierfTCUQ','8DoARSlv-HU'];
+const allVids = ['VyNWVdExM24','A66ierfTCUQ','8DoARSlv-HU'];
+const costanzaVids = ['A66ierfTCUQ','8DoARSlv-HU'];
+const kramerVids = ['VyNWVdExM24'];
 
 class Home extends Component {
   constructor(){
@@ -16,10 +18,17 @@ class Home extends Component {
 
   async componentDidMount(){
     // const videos = await youTube.search('3', 'trail running', 'video');
-    const videos = await youTube.getVideos(videoIds.join());
+    const videos = await youTube.getVideos(allVids.join());
     this.setState({
       videos: videos.result.items,
       loading: false
+    });
+  }
+
+  async filterVideos(ids){
+    const videos = await youTube.getVideos(ids.join());
+    this.setState({
+      videos: videos.result.items,
     });
   }
   
@@ -32,24 +41,23 @@ class Home extends Component {
       return <Loading />
     } else {
       content = videoResults.map(item => {
-        let src = (item.snippet.thumbnails.maxres ? item.snippet.thumbnails.maxres.url : item.snippet.thumbnails.medium.url);
+        const thumbnails = item.snippet.thumbnails;
+        const src = (thumbnails.maxres ? thumbnails.maxres.url : thumbnails.medium.url);
         return <Thumbnails 
                   key={item.id}
                   id={item.id}
                   title={item.snippet.title} 
                   src={src} 
                 />
-        // return  <EmbedVideo 
-        //           key={item.id}
-        //           id={item.id}
-        //           title={item.snippet.title} 
-        //         />
       });
     }
 
     return (
       <div className="content">
         <h1>Home</h1>
+        <button onClick={() => this.filterVideos(allVids)}>Show All</button>
+        <button onClick={() => this.filterVideos(costanzaVids)}>Costanza Filter</button>
+        <button onClick={() => this.filterVideos(kramerVids)}>Kramer Filter</button>
         {content}
       </div>
     );
