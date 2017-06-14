16d25a797f0d4ad7ec9d1b0e8a9f4ba2cdf57b4b
Matt Johnson <computermonkey@gmail.com>
2017-06-13 19:09:50 -0700

    move state to parent App component

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,40 @@
 import React from 'react';
 import {BrowserRouter as Router, Route, NavLink} from 'react-router-dom';
+import {youTube} from '../utils/gapi';
 import Home from './Home';
 import About from './About';
 import Video from './Video';
 
-const App = () => {
+const allVids = [
+  'https://www.youtube.com/watch?v=yg-TqEFYcfM',
+  'https://www.youtube.com/watch?v=jTi7VtrLL0k'
+];
+const costanzaVids = [
+  'https://www.youtube.com/watch?v=A66ierfTCUQ',
+  'https://www.youtube.com/watch?v=8DoARSlv-HU'
+];
+const kramerVids = [
+  'https://www.youtube.com/watch?v=VyNWVdExM24'
+];
+
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      videos: '',
+      loading: true,
+    }
+  }
+
+  async filterVideos(ids){
+    const videos = await youTube.getVideos(ids);
+    this.setState({
+      videos: videos.result.items,
+      loading: false,
+    });
+  }
+
+  render(){
     return (
       <Router>
         <div>
@@ -18,6 +48,7 @@ const App = () => {
         </div>
       </Router>
     );
+  }
 }
 
 export default App;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,49 +1,12 @@
 import React, { Component } from 'react';
-import {youTube} from '../utils/gapi';
 import Loading from './Loading';
 import Thumbnails from './Thumbnails';
 
-const allVids = [
-  'https://www.youtube.com/watch?v=yg-TqEFYcfM',
-  'https://www.youtube.com/watch?v=jTi7VtrLL0k'
-];
-const costanzaVids = [
-  'https://www.youtube.com/watch?v=A66ierfTCUQ',
-  'https://www.youtube.com/watch?v=8DoARSlv-HU'
-];
-const kramerVids = [
-  'https://www.youtube.com/watch?v=VyNWVdExM24'
-];
-
 class Home extends Component {
-  constructor(){
-    super();
-    this.state = {
-      videos: '',
-      loading: true,
-    }
-  }
-
-  async componentDidMount(){
-    // const videos = await youTube.search('3', 'trail running', 'video');
-    const videos = await youTube.getVideos(allVids);
-    this.setState({
-      videos: videos.result.items,
-      loading: false
-    });
-  }
-
-  async filterVideos(ids){
-    const videos = await youTube.getVideos(ids);
-    this.setState({
-      videos: videos.result.items,
-    });
-  }
-  
   render() {
     let content;
-    const videoResults = this.state.videos;
-    const loading = this.state.loading;
+    const videoResults = this.props.videos;
+    const loading = this.props.loading;
 
     if (loading === true) {
       return <Loading />
