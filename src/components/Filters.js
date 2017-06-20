caf4327c23e78b2e9b9c83c5194b3fd17035b9f6
Matt Johnson <matt@bamsas.com>
2017-06-20 15:33:58 -0700

    filter sections

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -2,12 +2,25 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import {filterList} from '../data';
 
+const printTitle = (group) => {
+  return <h2 key={group.name}>{group.name}</h2>
+}
+
+const printFilters = (group, filterVideos) => {
+    return group.filters.map(filter => {
+      return <button key={filter} data-filter={filter} onClick={() => filterVideos(filter)}>{filter}</button>
+    })
+}
+
 const Filters = ({filterVideos}) => {
   return(
     <div className='filters'>
       {
-        filterList.map(filter => {
-          return <button key={filter} data-filter={filter} onClick={() => filterVideos(filter)}>{filter}</button>
+        filterList.map(group => {
+          return ([
+            printTitle(group),
+            printFilters(group, filterVideos)
+          ])
         })
       }
     </div>
