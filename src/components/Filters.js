5a4b1e191dc6003605d9ba334258c647f54e18ad
Matt Johnson <matt@bamsas.com>
2017-06-20 16:11:30 -0700

    filter styling

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -2,17 +2,19 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import {filterList} from '../data';
 
-const printTitle = (group) => {
-  return <h2 key={group.name}>{group.name}</h2>
-}
-
-const printFilters = (group, filterVideos) => {
-    return group.filters.map(filter => {
-      return <button key={filter} data-filter={filter} onClick={() => filterVideos(filter)}>{filter}</button>
-    })
-}
-
 const Filters = ({filterVideos}) => {
+  const printTitle = (group) => {
+    if (group.name){
+      return <h3 key={group.name}>{group.name}</h3>
+    }
+  }
+
+  const printFilters = (group, filterVideos) => {
+      return group.filters.map(filter => {
+        return <button key={filter} data-filter={filter} onClick={() => filterVideos(filter)}>{filter}</button>
+      })
+  }
+
   return(
     <div className='filters'>
       {
