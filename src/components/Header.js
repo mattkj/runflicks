70d4f1e5fa9eba952ff802bd1879f82d05f5e41d
Matt Johnson <matt@bamsas.com>
2017-06-21 11:28:20 -0700

    link header back to home

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,14 @@
 import React from 'react';
 import {siteName, tagline} from '../data';
-// import {NavLink} from 'react-router-dom';
+import {Link} from 'react-router-dom';
 
 function Header() {
   return(
     <header>
-      <h1>{siteName}</h1>
+      <Link exact to="/">
+        <h1>{siteName}</h1>
+      </Link>
       <div>{tagline}</div>
-      {/*
-      <NavLink exact to="/">Home</NavLink>
-      <NavLink to="/about">About</NavLink>
-      */}
     </header>
   )
 }
