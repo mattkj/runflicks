6415b6c4aa95d974df2e056642731337613f42c2
Matt Johnson <matt@bamsas.com>
2017-06-21 15:08:16 -0700

    fix error in Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ import {Link} from 'react-router-dom';
 function Header() {
   return(
     <header>
-      <Link exact to="/">
+      <Link to="/">
         <h1>{siteName}</h1>
       </Link>
       <div>{tagline}</div>
