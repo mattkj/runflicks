e5d81058d207b4a760672ea09fab21919a0b5dfc
Matt Johnson <computermonkey@gmail.com>
2017-07-24 17:47:19 -0700

    update footer link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Footer() {
   return(
     <div className='credits'>
-      Made with &#9829; by <a href='https://github.com/mattkj'>Matt J</a>
+      Made with &#9829; by <a href='https://matt-johnson.com/'>Matt Johnson</a>
     </div>
   )
 }
